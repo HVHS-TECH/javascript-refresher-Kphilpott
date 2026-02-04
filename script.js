7c2d2e970c8d255a22106e12a325ebdd075409fe
Kphilpott <23424kp@hvhs.school.nz>
2026-02-05 10:20:03 +1300

    igygii&*^%*&%*%&&*&

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 console.log("ok it kinda works");
 
 function SendAlert() {
-   console.log("User Pressed the Button")
    alert("Congratulations!\nYou got the script running.");
+   
+   console.log("User Pressed the Button")
+   
+}
+
+function LeFunni() {
+   for (let i = 0; i < 5; i++) {
+    console.log("Unknown Error:", i);
+}
 }
